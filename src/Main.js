0164a3e077add09cd50d5a5bd240eb6464884b4f
Cassandra <umehcassandra@gmail.com>
2023-03-16 10:29:45 +0100

    add feature

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
+// import { BrowserRouter, Route } from 'react-router-dom';
+// import { Link } from 'react-router-dom'
 import Avatar from "./Avatar.png"
 import FacebookIcon from "./Assets/Facebook Icon.png"
 import GitHubIcon from "./Assets/GitHub Icon.png"
 import InstagramIcon from "./Assets/Instagram Icon.png"
 import TwitterIcon from "./Assets/Twitter Icon.png"
-
 
 function Main(){
     return(
@@ -15,13 +16,14 @@ function Main(){
                 <h3 className="name">Cassandra Umeh</h3>
                 <p className="role">Frontend developer</p>
                 <p className="contact">cassie.gmail.com</p>
+                
                 <div className="button">
-                <button className ="email-btn">Email</button>
-                <button className ="email-btn" style = {{backgroundColor: "#5093E2", color: "white"}} >LinkedIn</button>
+                <a href="mailto:umehcassandra@gmail.com"   style = {{backgroundColor: "#5093E2", color: "white"}}  className ="btn">Email</a>
+                <a href = "https://www.linkedin.com/in/cassandra-umeh/" aria-label="Linkedin" target="_blank" rel="noreferrer" style = {{backgroundColor: "#5093E2", color: "white"}} className = "btn">Linkedin</a>
                 </div>
+
                 <h4 className="about">About</h4>
-                <p className="about-paragraph">A motivated software engineering graduate from Altschool Africa with 6 months
-                of experience in software development. I am also an ardent open-source
+                <p className="about-paragraph">A motivated software engineering graduate from Altschool Africa with 1 year of experience in software development. I am also an ardent open-source
                 contributor and technical writer </p>
                  <h4 className="interest">Interests</h4>
                  <p className="interest-paragraph">Coding, resting, exercising.</p>
@@ -36,5 +38,8 @@ function Main(){
         </div>
     )
 }
+
+
+
 
 export default Main;
